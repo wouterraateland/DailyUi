56d065db925ee40e31dd449464c9df328c07d406
Wouter Raateland <wouterraateland@gmail.com>
2017-10-20 17:06:53 +0200

    Update styles to work on all screens. (change vw into vmin)

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -6,7 +6,7 @@ import Projects from 'components/Projects'
 const Home = () => (
   <div className="Home">
     <div className="Home-header">
-      <h1 className="Home-title">DailyUi challenge</h1>
+      <h1 className="Home-title">#DailyUi challenge</h1>
       <p className="Home-subtitle">By Wouter Raateland</p>
       <p className="Home-description">In 100 days, I will design 100 user interfaces. Some static, some interactive. Some simple some complex.</p>
       <div className="Home-cta">&darr;</div>
